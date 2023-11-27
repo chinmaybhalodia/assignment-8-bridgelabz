5e394a9cd31232e9fe80d13e43d035508ed360a8
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-27 22:05:45 +0530

    created method to compare 3 integers

--- MinMax.java
+++ MinMax.java
@@ -1,5 +1,15 @@
 public class MinMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Min-Max program.");
+
+        // testing UC1 for TC1 - TC3
+        System.out.println(getMaxInt(8, 7, 6));
+        System.out.println(getMaxInt(5, 6, 7));
+        System.out.println(getMaxInt(1, 4, 2));
+    }
+
+    // UC1: method compare 3 integers
+    public static int getMaxInt(int a, int b, int c) {
+        return a > (b > c ? b : c) ? a : (b > c ? b : c);
     }
 }
