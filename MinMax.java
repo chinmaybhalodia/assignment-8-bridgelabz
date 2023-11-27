c11a7d421e2369ea54628e93a1b069e6db392fc9
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-27 22:14:47 +0530

    created method to compare 3 double

--- MinMax.java
+++ MinMax.java
@@ -2,14 +2,19 @@ public class MinMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Min-Max program.");
 
-        // testing UC1 for TC1 - TC3
-        System.out.println(getMaxInt(8, 7, 6));
-        System.out.println(getMaxInt(5, 6, 7));
-        System.out.println(getMaxInt(1, 4, 2));
+        // testing UC2 for TC1 - TC3
+        System.out.println(getMaxDouble(8.6, 5.2, 4.3));
+        System.out.println(getMaxDouble(5.3, 6.9, 7.1));
+        System.out.println(getMaxDouble(1.5, 4.3, 2.8));
     }
 
     // UC1: method compare 3 integers
     public static int getMaxInt(int a, int b, int c) {
+        return a > (b > c ? b : c) ? a : (b > c ? b : c);
+    }
+
+    // UC2: method to compare 3 double
+    public static double getMaxDouble(double a, double b, double c) {
         return a > (b > c ? b : c) ? a : (b > c ? b : c);
     }
 }
